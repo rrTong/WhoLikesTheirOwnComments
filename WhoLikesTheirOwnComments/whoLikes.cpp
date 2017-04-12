6c0f9d632f32f1f46ae67aac5231e51663e1f401
rrTong <rrtongue@gmail.com>
2017-04-12 02:03:43 -0700

    Add files via upload

---
+++ WhoLikesTheirOwnComments/whoLikes.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int replyNum = 0;
+	int count;
+
+	cout << "Felix Cassidy is reply #0 \nWhat reply # are you? ";
+	cin >> replyNum;
+
+	cout << "\nCopy and paste this: \nwho actually comments on the fact";
+	for (count = 2; count < replyNum; count++)
+	{
+		cout << " that someone actually comments on the fact";
+	}
+	cout << " that someone likes their own comments\n";
+
+	system("pause");
+}
